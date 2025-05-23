c7f6e2cd3cd7cf38626e6e6fa4a4f7a5a6114fc1
Yekaterina Galochkina <galocichina.ecaterina@gmail.com>
2025-05-23 11:31:55 -0700

    Implementing buildingLetterPool helper function

--- src/adagrams.js
+++ src/adagrams.js
@@ -2,6 +2,26 @@ export const drawLetters = () => {
   // Implement this method for wave 1
 };
 
+
+////////// Helper function to built list of all letters //////////
+const buildingLetterPool = () => {
+  const letterPool = {
+    'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2,
+    'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4,
+    'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6,
+    'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1,
+    'Y': 2, 'Z': 1
+  };
+
+  let letters = '';
+  for (let [key,value] of Object.entries(letterPool)) {
+    letters += key.repeat(value);
+  }
+  
+  return letters.split('');
+}
+
+
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
 };
